562fddc33baaabd06290d612e547dbbf20dd6da5
Konstantin <OsmKos10@gmail.com>
2017-03-30 16:09:48 +0300

    Update product-seeder.js

--- seed/product-seeder.js
+++ seed/product-seeder.js
@@ -7,32 +7,32 @@ mongoose.connect('localhost:27017/shopping');
 var products = [
     new Product({
         imagePath: 'https://upload.wikimedia.org/wikipedia/en/5/5e/Gothiccover.png',
-        title: 'Gothic Video Game',
-        description: 'Awesome Game!!!!',
+        title: 'Gothic',
+        description: 'Awesome Game',
         price: 10
     }),
     new Product({
         imagePath: 'http://eu.blizzard.com/static/_images/games/wow/wallpapers/wall2/wall2-1440x900.jpg',
-        title: 'World of Warcraft Video Game',
-        description: 'Also awesome? But of course it was better in vanilla ...',
+        title: 'World of Warcraft ',
+        description: 'Also awesome',
         price: 20
     }),
     new Product({
         imagePath: 'https://support.activision.com/servlet/servlet.FileDownload?file=00PU000000Rq6tz',
         title: 'Call of Duty Video Game',
-        description: 'Meh ... nah, it\'s okay I guess',
+        description: 'Not good enough',
         price: 40
     }),
     new Product({
         imagePath: 'https://pmcdeadline2.files.wordpress.com/2014/02/minecraft__140227211000.jpg',
-        title: 'Minecraft Video Game',
-        description: 'Now that is super awesome!',
+        title: 'Minecraft ',
+        description: 'shit',
         price: 15
     }),
     new Product({
         imagePath: 'https://d1r7xvmnymv7kg.cloudfront.net/sites_products/darksouls3/assets/img/DARKSOUL_facebook_mini.jpg',
-        title: 'Dark Souls 3 Video Game',
-        description: 'I died!',
+        title: 'Dark Souls 3 ',
+        description: '2 easy 2 die',
         price: 50
     })
 ];
